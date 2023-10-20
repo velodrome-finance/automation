aba60df7f68764e0893a6912b753d350879837ee
Ricardo Airton Silva <ricardoairtonsilva@hotmail.com>
2023-10-20 19:54:35 +0100

    chore: add velodrome library contract

--- web3-functions/relay-automation/utils/constants.ts
+++ web3-functions/relay-automation/utils/constants.ts
@@ -5,6 +5,7 @@ import { BigNumber } from "@ethersproject/bignumber";
 
 import lp_sugar_abi from "../abis/lp_sugar.json";
 
+export const VELO_LIBRARY_ADDRESS = "0x253CA289Cd921ba4a18C053C00a80c9660D508f8";
 export const LP_SUGAR_ADDRESS = "0x4d996e294b00ce8287c16a2b9a4e637eca5c939f";
 export const ROUTER_ADDRESS = "0xa062ae8a9c5e11aaa026fc2670b0d65ccc8b2858";
 export const VELO = "0x9560e827aF36c94D2Ac33a39bCE1Fe78631088Db";
