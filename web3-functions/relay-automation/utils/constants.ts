26279c8b4724f7a088dd7493ce3f5ebb2c4bf246
Ricardo Airton Silva <ricardoairtonsilva@hotmail.com>
2023-11-02 16:55:30 +0000

    chore: update to latest lp sugar

--- web3-functions/relay-automation/utils/constants.ts
+++ web3-functions/relay-automation/utils/constants.ts
@@ -5,7 +5,7 @@ import { BigNumber } from "@ethersproject/bignumber";
 
 import lp_sugar_abi from "../abis/lp_sugar.json";
 
-export const LP_SUGAR_ADDRESS = "0x4d996e294b00ce8287c16a2b9a4e637eca5c939f";
+export const LP_SUGAR_ADDRESS = "0x6eDCAb198EAdDBDA3865f813A83F6bC9012F16e9";
 export const ROUTER_ADDRESS = "0xa062ae8a9c5e11aaa026fc2670b0d65ccc8b2858";
 export const VELO = "0x9560e827aF36c94D2Ac33a39bCE1Fe78631088Db";
 export const KEEPER_REGISTRY_ADDRESS =
